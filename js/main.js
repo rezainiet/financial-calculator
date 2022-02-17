2c50b5a8f549c6d5af3aade316e0b83d46ed53de
rezainiet <masudrezaog@gmail.com>
2022-02-17 21:47:02 +0600

    simple design added

--- js/main.js
+++ js/main.js
@@ -1,73 +1,83 @@
-function getSignleExpenseAmount(inputId) {
-    const foodInput = document.getElementById(inputId);
-    const foodAmountText = foodInput.value;
-    const foodAmount = parseFloat(foodAmountText);
-    foodInput.value = '';
-    return foodAmount;
+// get input value function
+function getInputValue(id) {
+    const numberInput = document.getElementById(id);
+    const inputAmount = parseFloat(numberInput.value);
+    numberInput.value = '';
+    return inputAmount;
 }
 
-document.getElementById('calculate-button').addEventListener('click', function (inputId) {
+// event handler of calculate
+document.getElementById('calculate-button').addEventListener('click', function (e) {
+    // income amount 
+    const incomeInput = document.getElementById("income-input");
+    const incomeAmount = parseFloat(incomeInput.value);
+    // food amount
+    const foodAmount = getInputValue('food-input');
+    // rent amount 
+    const rentAmount = getInputValue('rent-input');
+    // clothes amount
+    const clothesAmount = getInputValue('clothes-input');
+    if (foodAmount > 0 && rentAmount > 0 && clothesAmount > 0) {
+        document.getElementById('calculate-error').style.display = 'none';
+        const expenseTotal = foodAmount + rentAmount + clothesAmount;
+        if (incomeAmount < expenseTotal) {
+            document.getElementById('insufficient-balance').innerText = 'Insufficient Income Balance!!';
+        }
+        else {
+            const totalExpenseText = document.getElementById('total-expense');
+            totalExpenseText.innerText = expenseTotal;
+            const balanceText = document.getElementById('balance');
+            const updateBalance = incomeAmount - expenseTotal;
+            balanceText.innerText = updateBalance;
+        }
+    }
+    // error handeling
+    else {
+        const getCalculateError = document.getElementById('calculate-error');
+        getCalculateError.innerText = 'Please enter the valid number!!'
+    }
+})
 
-    // Food Input Value
-    foodAmount = getSignleExpenseAmount('foodInput')
-    // console.log(foodAmount);
-
-    // Rent Input Value
-    rentAmount = getSignleExpenseAmount('rentInput')
-    // console.log(rentAmount);
-
-    // Clothes Input Value
-    clothesAmount = getSignleExpenseAmount('clothesInput')
-    // console.log(clothesAmount);
-
-
-    // Total Expenses Amount
-    const totalExpenses = document.getElementById('total-expenses');
-    const totalExpenseText = totalExpenses.innerText;
-    const totalExpenseAmount = parseFloat(totalExpenseText);
-
-    // Total of Total expenses
-    const totalOfTotalExpenses = totalExpenses.innerText = foodAmount + rentAmount + clothesAmount;
-
-    // Income Input
-    const incomeInput = document.getElementById('incomeInput');
-    const incomeAmountText = incomeInput.value;
-    const incomeAmount = parseFloat(incomeAmountText);
-
-    // Balance
-    const balanceText = document.getElementById('balance-text');
-    const balanceAmountText = balanceText.innerText;
-    const balanceAmount = parseFloat(balanceAmountText);
-
-    balanceText.innerText = incomeAmount - totalOfTotalExpenses;
-});
-
-// (input * balance) / 100
-document.getElementById('save-button').addEventListener('click', function () {
-
-    // Balance
-    const balanceText = document.getElementById('balance-text');
-    const balanceAmountText = balanceText.innerText;
-    const balanceAmount = parseFloat(balanceAmountText);
-
-    // Save Input field
-    const saveInput = document.getElementById('save-input');
-    const saveInputAmountText = saveInput.value;
-    const saveInputAmount = parseFloat(saveInputAmountText);
-    // console.log(saveInputAmount);
-
-    // Saving Amount
-    const getSavingAmount = document.getElementById('saving-amount');
-    const savingAmountText = getSavingAmount.innerText;
-    const savingAmount = parseFloat(savingAmountText);
-    // console.log(savingAmount);
-
-    // Remaining Balance
-    const getRemainingBalance = document.getElementById('remaining-balance');
-    const remainingBalanceText = getRemainingBalance.innerText;
-    const remainingBalance = parseFloat(remainingBalanceText);
-    // console.log('This is remaing balance', remainingBalance);
-
-    const newSavingAmount = getSavingAmount.innerText = (saveInputAmount * balanceAmount) / 100;
-    getRemainingBalance.innerText = balanceAmount - newSavingAmount;
+// event handler of saving
+document.getElementById('save-button').addEventListener('click', function (e) {
+    // get saving input 
+    const savingInput = getInputValue('save-input');
+    // income amount 
+    const incomeInput = document.getElementById("income-input");
+    const incomeAmount = parseFloat(incomeInput.value);
+    // get balance text 
+    const balanceText = document.getElementById('balance');
+    const balance = parseFloat(balanceText.innerText);
+    if (savingInput > 0) {
+        // document.getElementById('save-error').style.display = "none";
+        // get saving percentage
+        if (savingInput <= 100) {
+            const savingTotal = incomeAmount / 100 * savingInput;
+            if (balance >= savingTotal) {
+                document.getElementById('save-error').style.display = "none";
+                const savingAmountText = document.getElementById('saving-amount');
+                savingAmountText.innerText = savingTotal;
+                // get remaing balance text 
+                const remainingBalanceText = document.getElementById('remaining-balance');
+                // calculate remaining balance
+                const remainingBalance = balance - savingTotal;
+                remainingBalanceText.innerText = remainingBalance;
+            }
+            // error handeling
+            else {
+                document.getElementById('save-error').style.display = "block";
+                document.getElementById('save-error').innerText = " Your Saving amount can't be bigger than your main balance";
+            }
+        }
+        // error handeling
+        else {
+            document.getElementById('save-error').style.display = "block";
+            document.getElementById('save-error').innerText = "Please enter a number smaller than 100";
+        }
+    }
+    // error handeling
+    else {
+        document.getElementById('save-error').style.display = "block";
+        document.getElementById('save-error').innerText = "Please enter the valid number!!";
+    }
 })
