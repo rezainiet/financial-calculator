2ac48c47ab01d63858a6cc458fb926bd6eaf0eb7
rezainiet <masudrezaog@gmail.com>
2022-02-17 22:20:15 +0600

    final commit

--- js/main.js
+++ js/main.js
@@ -8,14 +8,17 @@ function getInputValue(id) {
 
 // event handler of calculate
 document.getElementById('calculate-button').addEventListener('click', function (e) {
-    // income amount 
+    // Income Aamount 
     const incomeInput = document.getElementById("income-input");
     const incomeAmount = parseFloat(incomeInput.value);
-    // food amount
+
+    // Food Amount
     const foodAmount = getInputValue('food-input');
-    // rent amount 
+
+    // Rent Amount 
     const rentAmount = getInputValue('rent-input');
-    // clothes amount
+
+    // Clothes Amount
     const clothesAmount = getInputValue('clothes-input');
     if (foodAmount > 0 && rentAmount > 0 && clothesAmount > 0) {
         document.getElementById('calculate-error').style.display = 'none';
@@ -31,53 +34,55 @@ document.getElementById('calculate-button').addEventListener('click', function (
             balanceText.innerText = updateBalance;
         }
     }
-    // error handeling
+    // Error Handling
     else {
         const getCalculateError = document.getElementById('calculate-error');
-        getCalculateError.innerText = 'Please enter the valid number!!'
+        getCalculateError.innerText = 'Please enter a valid number!'
     }
 })
 
 // event handler of saving
 document.getElementById('save-button').addEventListener('click', function (e) {
-    // get saving input 
+    // Get saving input 
     const savingInput = getInputValue('save-input');
-    // income amount 
+    // Income amount 
     const incomeInput = document.getElementById("income-input");
     const incomeAmount = parseFloat(incomeInput.value);
     // get balance text 
     const balanceText = document.getElementById('balance');
     const balance = parseFloat(balanceText.innerText);
     if (savingInput > 0) {
-        // document.getElementById('save-error').style.display = "none";
-        // get saving percentage
+        // Get Saving Percentage
         if (savingInput <= 100) {
             const savingTotal = incomeAmount / 100 * savingInput;
             if (balance >= savingTotal) {
                 document.getElementById('save-error').style.display = "none";
                 const savingAmountText = document.getElementById('saving-amount');
                 savingAmountText.innerText = savingTotal;
-                // get remaing balance text 
+                
+                // Get remaining balance text 
                 const remainingBalanceText = document.getElementById('remaining-balance');
-                // calculate remaining balance
+
+                // Calculation of remaining balance!
                 const remainingBalance = balance - savingTotal;
                 remainingBalanceText.innerText = remainingBalance;
             }
-            // error handeling
+            // Error handling
             else {
                 document.getElementById('save-error').style.display = "block";
                 document.getElementById('save-error').innerText = " Your Saving amount can't be bigger than your main balance";
             }
         }
-        // error handeling
+        // Error handling
         else {
             document.getElementById('save-error').style.display = "block";
             document.getElementById('save-error').innerText = "Please enter a number smaller than 100";
         }
     }
-    // error handeling
+
+    // Error handling
     else {
         document.getElementById('save-error').style.display = "block";
-        document.getElementById('save-error').innerText = "Please enter the valid number!!";
+        document.getElementById('save-error').innerText = "Please enter a valid number!";
     }
 })
